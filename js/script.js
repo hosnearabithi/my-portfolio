d1ac0d1415733eca22d4d23abd6a30626404ddb2
Hosne Ara Bithi <hosnearabithi20@gamil.com>
2023-03-03 18:01:54 +0600

    updated

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // Typing Script JS
 var typed = new Typed(".typing",{
-    strings: ["Web Designer", "Web Developer", "Youtuber", "Blogger"],
+    strings: ["Web Designer", "Web Developer"],
     typeSpeed: 80,
     backSpeed: 80,
 });
 
 var typed = new Typed(".typing-2",{
-    strings: ["Web Developer", "Web Designer", "Youtuber", "Blogger"],
+    strings: ["Web Developer", "Web Designer"],
     typeSpeed: 100,
     backSpeed: 60,
 });
